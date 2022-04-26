e63727cc235cc2cd2a4a1c355887a5238462094e
Krisztian <vargakrisz07@gmail.com>
2022-04-26 15:03:36 +0200

    First commmit

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,24 @@
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         try {
-            File myObj = new File("number.txt").getAbsoluteFile();
-            Scanner myReader = new Scanner(myObj);
+            URL myObj = Main.class.getResource("number.txt");
+            Scanner myReader = new Scanner(myObj.openStream());
             com.sun.security.auth.module.NTSystem NTSystem = new com.sun.security.auth.module.NTSystem();
             String data = myReader.nextLine();
             System.out.println(NTSystem.getName() + ": " + data);
             myReader.close();
-            FileWriter writer = new FileWriter(myObj, false);
+           /* FileWriter writer = new FileWriter(myObj.openStream(), false);
             int number = Integer.parseInt(data);
             number++;
             writer.write(Integer.toString(number));
-            writer.close();
+            writer.close();*/
 
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
