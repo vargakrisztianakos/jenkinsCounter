e3588804116f99a0484fbaa8af568afa1eabe29e
Krisztian <vargakrisz07@gmail.com>
2022-05-05 21:17:56 +0200

    Main has changed to handle multiple os

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main {
@@ -12,11 +13,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         try {
             URL myObj = Main.class.getResource("number.txt");
-            System.out.println(myObj);
             Scanner myReader = new Scanner(myObj.openStream());
-            com.sun.security.auth.module.NTSystem NTSystem = new com.sun.security.auth.module.NTSystem();
             String data = myReader.nextLine();
-            System.out.println(NTSystem.getName() + ": " + data);
+            if (System.getProperty("os.name") == "Linux"){
+                com.sun.security.auth.module.UnixSystem USystem = new com.sun.security.auth.module.UnixSystem();
+                System.out.println(USystem.getUsername() + ":" + data);
+            }else {
+                com.sun.security.auth.module.NTSystem NTSystem = new com.sun.security.auth.module.NTSystem();
+                System.out.println(NTSystem.getName() + ": " + data);
+            }
             myReader.close();
             FileWriter writer = new FileWriter(myObj.getPath(), false);
             int number = Integer.parseInt(data);
